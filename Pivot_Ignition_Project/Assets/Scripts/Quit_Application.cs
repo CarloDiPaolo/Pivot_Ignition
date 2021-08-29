7f472beba1c45b5dc6924fa4158fe2087e89f903
Carlo di Paolo <carlodipaolo94@gmail.com>
2021-08-29 19:20:08 +0200

    Added Quit Application. Finished with this for now, might resume in the future.

---
+++ Pivot_Ignition_Project/Assets/Scripts/Quit_Application.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit_Application : MonoBehaviour
+{
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+            Debug.Log("Quit Apllication");
+        }
+    }
+}
